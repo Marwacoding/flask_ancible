b29f24310a3bd627c72798c2c84eb5a4f39edc5f
Marwa <majbrim@gmail.com>
2021-03-09 11:59:16 +0100

    create table and insert into it id

--- postgres.py
+++ postgres.py
@@ -16,7 +16,6 @@ password = os.environ["password"]
 sslmode = "require" 
 
 conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
-#print(conn_string)
 conn = psycopg2.connect(conn_string)
 cursor = conn.cursor()
 
@@ -31,3 +30,19 @@ def create_table():
     logging.info("[POSTGRES] Creation table : end")
 
 create_table()
+
+def insert_id():
+    logging.info("[POSTGRES] Insert data in table : start")
+
+    try:
+        insert = "INSERT INTO my_table (id) values(default)"
+        #print('value of carpet table',value)
+        cursor.execute(insert)
+
+        conn.commit()
+    except Exception as e:
+        logging.error('[POSTGRES] ERROOOOR !! empty list, didnt file table' +str(e))
+
+        logging.info("[POSTGRES] Insert data in table : end")
+
+insert_id()
